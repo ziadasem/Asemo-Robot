390a11bab8c502d72a3fcd04d1004772d098d7f0
ziad asem <zeyadstudio@gmail.com>
2020-07-25 20:18:45 +0200

    adding comments to odom publisher node

--- odom_pub/src/odom_pub.cpp
+++ odom_pub/src/odom_pub.cpp
@@ -1,10 +1,15 @@
+//INCLUSIONS OF DATA TYPES
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Point32.h>
+
+//i dont know -ABCDE-
 #define R 0.05
 #define N 725.0
 #define L 0.33
+
+//installation
 double Dc=0.0;
 double RtickOld=0.0;
 double RtickNew=0.0;
@@ -14,32 +19,40 @@ double x = 0.0;
 double y = 0.0;
 double th = 0.0;
 
+// velocity init
 double vx = 0.0;
 double vy = 0.0;
 double vth = 0.0;
 
+// for every new message published on '/odom' topic, this function is triggered and the message is passed as 'msg' variable name
 void encoder_cb(const geometry_msgs::Point32& msg)
 {
-RtickNew=(double)msg.x;//encoder
-LtickNew=(double)msg.y;
-Dc=((2*3.14*R*(RtickNew-RtickOld)/N)+((2*3.14*R*(LtickNew-LtickOld)/N)))/2;
-x+=Dc*cos(th);
-y+=Dc*sin(th);
-th+=((2*3.14*R*(RtickNew-RtickOld)/N)-((2*3.14*R*(LtickNew-LtickOld)/N)))/L;
+  //data from encoder
+RtickNew=(double)msg.x;  //encoder //the right wheel is defined as X from arduino node file
+LtickNew=(double)msg.y; //the left wheel is defined as Y from arduino node file
+  
+Dc=((2*3.14*R*(RtickNew-RtickOld)/N)+((2*3.14*R*(LtickNew-LtickOld)/N)))/2; //distance calculation formula
+x+=Dc*cos(th); //adding the new distance in x-direction
+y+=Dc*sin(th); //adding the new distance in y-direction
+th+=((2*3.14*R*(RtickNew-RtickOld)/N)-((2*3.14*R*(LtickNew-LtickOld)/N)))/L; //adding the new angle 
+
+//updating tickes
 RtickOld=RtickNew;
 LtickOld=LtickNew;
 }
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "odometry_publisher");
-  ros::NodeHandle n;
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
-  tf::TransformBroadcaster odom_broadcaster;
-  ros::Subscriber sub = n.subscribe("/encoder", 1000, encoder_cb);
-// Here I need to Insert the subscriber part that subscibes to arduino topics and get the encoder reading then pose rate from pose
+  ros::init(argc, argv, "odometry_publisher"); // init the node with name odometry_publisher and two necessary arguments argc argv
+  ros::NodeHandle n;  // all opreations on node done through this instance
 
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50); // telling master we will publish nav_msgs::Odometry to 'odom topic' with publishing queue size 50
+  tf::TransformBroadcaster odom_broadcaster; //establish odom publisher
+  
+  ros::Subscriber sub = n.subscribe("/encoder", 1000, encoder_cb);// telling master we will subscribe data from 'odom encoder' with publishing queue size 1000 and the callback function
 
-  ros::Time current_time, last_time;
+  //Here I need to Insert the subscriber part that subscibes to arduino topics and get the encoder reading then pose rate from pose
+  
+  ros::Time current_time, last_time; //init time to be passed to odom.header.stamp
   current_time = ros::Time::now();
   last_time = ros::Time::now();
 
@@ -56,14 +69,16 @@ int main(int argc, char** argv){
     vx=Dc*cos(th)/dt;
     vy=Dc*sin(th)/dt;
     vth=(((2*3.14*R*(RtickNew-RtickOld)/N)-((2*3.14*R*(LtickNew-LtickOld)/N)))/L)/dt;
-   /* double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+  
+        /* double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
     double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
     double delta_th = vth * dt;
 
     x += delta_x;
     y += delta_y;
     th += delta_th;*/
-// here i can change the algorithm as i know the distance but not the velocity so i can divide instead of multiply and also i have to consider the
+    
+        // here i can change the algorithm as i know the distance but not the velocity so i can divide instead of multiply and also i have to consider the
     //sampling and data input from arduino to C++ node
     //since all odometry is 6DOF we'll need a quaternion created from yaw
     geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
